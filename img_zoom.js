599bad12fa19ee3fd988eb80b7b352b09f3a1733
supersiem <vankeulensiem@gmail.com>
2024-06-08 19:25:00 +0200

    haha pasta go brr

--- img_zoom.js
+++ img_zoom.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const zoomImage = zoomContainer.querySelector('.zoom-image');
 
     zoomContainer.addEventListener('mousemove', (event) => {
-      const rect = zoomContainer.getBoundingClientRect();
-      const x = (event.clientX - rect.left) / zoomContainer.offsetWidth;
-      const y = (event.clientY - rect.top) / zoomContainer.offsetHeight;
-      zoomImage.style.transformOrigin = `${x * 100}% ${y * 100}%`;
+        const rect = zoomContainer.getBoundingClientRect();
+        const x = (event.clientX - rect.left) / zoomContainer.offsetWidth;
+        const y = (event.clientY - rect.top) / zoomContainer.offsetHeight;
+        zoomImage.style.transformOrigin = `${x * 100}% ${y * 100}%`;
     });
-  });
+});
