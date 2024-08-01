e0f52d50618e234936c57ff815597ffbe0855d0e
supersiem <vankeulensiem@gmail.com>
2024-08-01 17:17:11 +0200

    pindakaas

---
+++ login_manager.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', async function (event) {
+    var acount_text = document.getElementById("acount_text");
+    console.log(in_gelogd())
+    if (in_gelogd()) {
+        acount_text.innerText = await acount_naam()
+    }
+
+
+});
+
+function in_gelogd() {
+    if (localStorage.getItem("key") != "null") {
+        return true
+    }
+    return false
+}
+async function acount_naam() {
+    const options = {method: 'GET'};
+
+    return await makeRequest("http://127.0.0.1:5000/get_item/user_key/"+localStorage.getItem("key")+"/1")
+}
+async function makeRequest(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.text();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
