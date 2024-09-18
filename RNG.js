2c4ad23ba53566688bf35f9d7bb7189bde32072b
supersiem <100954549+supersiem@users.noreply.github.com>
2024-09-18 14:29:39 +0200

    hotfix beta 1.0071

--- RNG.js
+++ RNG.js
@@ -17,7 +17,7 @@ function RNG(daily) {
         var temp = "";
         const daily_seeds = [];
         for (let loop = 1; loop < 6; console.log(loop)) {
-            temp = Math.floor(Math.random() * info.foto_hoeveelheid);
+            temp = Math.floor(Math.random() * info.foto_hoeveelheid) + 1;
             if (!daily_seeds.includes(temp)) {
                 daily_seeds[loop] = temp;
                 sessionStorage.setItem("DAILY_" + loop, temp);
